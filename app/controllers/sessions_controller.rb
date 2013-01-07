7c458b15eeb31d45db27df53548bc09283d5e12e
root <gf004930@etu.unice.fr>
2013-01-07 23:59:23 +0100

    tests signin with invalid information passés au vert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    author = Author.find_by_email(params[:session][:email].downcase)
+    if author && author.authenticate(params[:session][:password])
+      redirect_to author
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
